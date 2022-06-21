0936781104dfdfe032989c8078cca77ad3f0dab4
Yaroslav Petrukhin <75678049+YarPetru@users.noreply.github.com>
2022-06-21 20:46:07 +0300

    refactoring to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,78 @@
 import React, { Component } from 'react';
-// import * as API from './api/api';
+import * as API from './api/api';
 import { ToastContainer } from 'react-toastify';
+// import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
 import Modal from './Modal';
+import Loader from './Loader';
+import Button from './Button';
 
 export class App extends Component {
   state = {
     queryWord: '',
+    pics: [],
+    error: null,
+    activeId: null,
+    status: 'idle',
+    page: 1,
     showModal: false,
     pickedPicture: null,
   };
 
+  async componentDidUpdate(prevProps, prevState) {
+    // console.log(`active img is ${this.state.activeId}`);
+    if (prevState.queryWord !== this.state.queryWord) {
+      // console.log(this.props.query);
+      // console.log(prevProps.query);
+      try {
+        this.setState({ status: 'pending' });
+        const pics = await API.getPictures(
+          this.state.queryWord,
+          this.state.page
+        );
+        this.setState({ pics, status: 'resolved' });
+      } catch (error) {
+        this.setState({ error, status: 'rejected' });
+        console.log(error);
+      }
+    }
+
+    if (prevState.page !== this.state.page && this.state.page > 1) {
+      try {
+        this.setState({ status: 'pending' });
+        const newPics = await API.getPictures(
+          this.state.queryWord,
+          this.state.page
+        );
+
+        this.setState(prevState => ({
+          pics: [...prevState.pics, ...newPics],
+          status: 'resolved',
+        }));
+      } catch (error) {
+        this.setState({ error, status: 'rejected' });
+        console.log(error);
+      }
+    }
+    this.makeScroll();
+  }
+
   handleFormSubmit = queryWord => {
     this.setState({ queryWord });
+  };
+
+  makeScroll = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   openModal = e => {
@@ -28,16 +85,36 @@ export class App extends Component {
   };
 
   render() {
-    const { queryWord, showModal, pickedPicture } = this.state;
+    const { pics, status, error, showModal, pickedPicture } = this.state;
+
+    // if (status === 'idle') {
+    //   return <p>Enter your query</p>;
+    // }
+
+    // if (status === 'pending') {
+    //   return <Loader />;
+    // }
+
+    if (status === 'rejected') {
+      return (
+        <p>{`Oops. Something went wrong :( Please try again:${error.message}`}</p>
+      );
+    }
+
     return (
       <div className="container">
         <Searchbar onGetWord={this.handleFormSubmit} />
 
-        <ImageGallery
-          // pictures={pics}
-          query={queryWord}
-          onItemClick={this.openModal}
-        />
+        {status === 'idle' && <p>Enter your query</p>}
+
+        {status === 'pending' && <Loader />}
+
+        {status === 'resolved' && pics.length > 0 && (
+          <>
+            <ImageGallery pics={pics} onItemClick={this.openModal} />
+            <Button onClick={this.handleLoadMore} />
+          </>
+        )}
 
         {showModal && (
           <Modal
@@ -52,3 +129,16 @@ export class App extends Component {
     );
   }
 }
+
+// {
+//       toast.warn(`Нет картинок соответствующих запросу ${this.props.query}`, {
+//         theme: 'colored',
+//         // ПОЧЕМУ ДВА РАЗ ПОЯВЛЯЕТСЯ, может лучше обработать в другом месте
+//       });
+//     }
+
+// if (!this.state.pics) {
+//   return Promise.reject(
+//     new Error(`нет картинок соответствующих запросу ${this.props.query}`)
+//   );
+// }
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -8,13 +8,15 @@ const Loader = () => {
   };
 
   return (
-    <ThreeDots
-      style={style}
-      height="100"
-      width="100"
-      color="#3f51b5"
-      ariaLabel="loading"
-    />
+    <div style={style}>
+      <ThreeDots
+        style={style}
+        height="100"
+        width="100"
+        color="#3f51b5"
+        ariaLabel="loading"
+      />
+    </div>
   );
 };
 
